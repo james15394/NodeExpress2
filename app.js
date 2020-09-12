06bf0e61a56e0591cfb23fad1f601e2409df02ca
Duong <d1230310024@gmail.com>
2020-09-12 21:46:41 +0700

    Add authRoute

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const mongoose = require('mongoose');
+const authRoute = require('./routes/authRoutes');
 require('dotenv').config();
 const app = express();
 app.set('view engine', 'ejs');
-
-mongoose.connect(process.env.MONGO_SECRET, {useNewUrlParser: true, useUnifiedTopology: true});
 app.use(express.json());
 app.use(express.static('public'));
+app.use(authRoute);
+
+mongoose.connect(process.env.MONGO_SECRET, {useNewUrlParser: true, useUnifiedTopology: true});
+
 //get
 app.get('/', (req, res) => {
     res.render('signup');
