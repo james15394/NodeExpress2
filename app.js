7b8838a1f2b28e46ce96f6ce5bf4f1394eb6711e
Duong <d1230310024@gmail.com>
2020-09-12 21:28:42 +0700

    Add views

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const mongoose = require('mongoose');
-
+require('dotenv').config();
 const app = express();
+app.set('view engine', 'ejs');
 
-
+mongoose.connect(process.env.MONGO_SECRET, {useNewUrlParser: true, useUnifiedTopology: true});
+app.use(express.json());
+app.use(express.static('public'));
+//get
 app.get('/', (req, res) => {
-    res.send('Hello')
-})
+    res.render('signup');
+});
+//post
+
 app.listen(3000);
