6781e2c76461c42ae2f81cf667accfd48bfcd8f3
Duong <d1230310024@gmail.com>
2020-09-18 07:48:14 +0700

    Update authControllers

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,25 @@
 const User = require('../model/user');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 //handling errors
 const handlingErr = (err) => {
     const error = {name: "", email: "", password: ""};
+    if(err.message == "Incorrect password") {
+        error.password = "Incorrect Password"
+    }
+    if(err.message == "Incorrect Email") {
+        error.email = "Incorrect Email"
+    }
     if(err.message.includes("user validation failed")) {
     Object.values(err.errors).forEach(({ properties }) => {
+        if(properties.type =="unique" && properties.path == "name" ) {
+            error.name = "Username already in use"
+        } else if (properties.type =="unique" && properties.path == "email") {
+            error.email = "Email already in use"
+        } else {
         error[properties.path] = properties.message;
+        }
     })
     }
     return error;
@@ -15,9 +28,11 @@ const handlingErr = (err) => {
 const jwtSign = (userId) => {
     return jwt.sign({ userId }, process.env.ACCESS_TOKEN)
 }
+
 module.exports.signup_get = (req, res) => {
     res.render('signup', { title: "Sign up" })
 }
+
 module.exports.signup_post = async (req, res) => {
     const {name, email, password} = req.body;
     try {
@@ -30,3 +45,21 @@ module.exports.signup_post = async (req, res) => {
         res.status(400).json({ showErr })
     }
 }
+
+module.exports.login_get = (req, res) => {
+    res.render('login', { title: "Log in"})
+}
+
+module.exports.login_post = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.login(email, password);
+        const accessToken = jwtSign(user._id);
+        res.cookie('jwt', accessToken)
+        res.status(200).json({ user: user._id })
+    } catch(err) {
+        const error = handlingErr(err);
+        res.status(400).json({ error });
+    }
+}
+
